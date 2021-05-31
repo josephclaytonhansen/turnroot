3437ae7c34f99dcc53d49b3db73e4dc1960aaf63
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-05-31 09:00:50 -0600

    bugfix unit growth rate test

--- src/UI_unit_editor_more_dialogs.py
+++ src/UI_unit_editor_more_dialogs.py
@@ -34,6 +34,8 @@ class testGrowthDialog(QDialog):
         
         self.rt = 0
         self.l = 0
+        self.total_l = 0
+        self.rolls = 0
         
         for s in universal_stats:
             self.stat_amounts[s] = 0
@@ -54,15 +56,19 @@ class testGrowthDialog(QDialog):
         clear.clicked.connect(self.reset)
         clear.setFont(self.body_font)
         
-        self.test_runs = QLabel("Levelled Up "+str(self.rt)+" Times")
+        self.test_runs = QLabel("Leveled Up "+str(self.rt)+" Times")
         self.test_runs.setFont(self.body_font)
-        self.likely = QLabel("Based on current rates, "+str(self.l)+"% of level ups\nwill have the same stat gain as this level up")
+        self.likely = QLabel("This level up is ~"+str(self.l)+"% likely")
         self.likely.setFont(self.body_font)
         
+        self.total_likely = QLabel("The chance of "+str(self.rt)+" level ups having these total values is "+str(self.total_l)+"%")
+        self.total_likely.setFont(self.body_font)
+        
         self.layout.addWidget(test, row_count+1,  0)
-        self.layout.addWidget(clear,row_count+3,0)
+        self.layout.addWidget(clear,row_count+4,0)
         self.layout.addWidget(self.test_runs,row_count+1,1)
         self.layout.addWidget(self.likely,row_count+2,0,1,2)
+        self.layout.addWidget(self.total_likely,row_count+3,0,1,2)
     
     def reset(self):
         self.stat_amounts = {}
@@ -71,9 +77,12 @@ class testGrowthDialog(QDialog):
             self.amounts[s].setText("+"+str(self.stat_amounts[s]))
             self.amounts[s].setStyleSheet("background-color: "+self.active_theme.window_background_color+";color: "+self.active_theme.window_text_color)
             self.rt= 0
-            self.test_runs.setText("Levelled Up "+str(self.rt)+" Times")
+            self.test_runs.setText("Leveled Up "+str(self.rt)+" Times")
             self.l = 0
-            self.likely.setText("Based on current rates, this level up is "+str(self.l)+"% likely")
+            self.rolls = 0
+            self.total_l = 0
+            self.likely.setText("This level up is ~"+str(self.l)+"% likely")
+            self.total_likely.setText("The chance of "+str(self.rt)+" level ups having these total values is "+str(self.total_l)+"%")
     
     def run_test(self):
         self.unit = self.parent.parent.unit
@@ -105,7 +114,7 @@ class testGrowthDialog(QDialog):
             else:
                 self.amounts[s].setStyleSheet("background-color: "+self.active_theme.window_background_color+";color: "+self.active_theme.window_text_color)
         self.rt += 1
-        self.test_runs.setText("Levelled Up "+str(self.rt)+" Times")
+        self.test_runs.setText("Leveled Up "+str(self.rt)+" Times")
         
         prob = 0
         for s in actual_outcomes:
@@ -115,9 +124,8 @@ class testGrowthDialog(QDialog):
         prob = prob / len(actual_outcomes)
         prob = int(prob * 100)
         self.l = prob
+        self.rolls += self.l
+        self.total_l = int(self.rolls / self.rt)
         
-        self.likely.setText("Based on current rates, this level up is "+str(self.l)+"% likely")
-                
-            
-            
-        
+        self.likely.setText("This level up is ~"+str(self.l)+"% likely")
+        self.total_likely.setText("The chance of "+str(self.rt)+" level ups having these total values is "+str(self.total_l)+"%")
