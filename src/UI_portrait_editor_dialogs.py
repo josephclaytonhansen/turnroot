7158848b19eeb77358da042091974e0ea849bdb5
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-06-11 11:57:58 -0600

    more portrait editor layer button work

--- src/UI_portrait_editor_dialogs.py
+++ src/UI_portrait_editor_dialogs.py
@@ -31,8 +31,8 @@ class portraitStackWidget(QWidget):
         self.layout.setSpacing(2)
         self.setLayout(self.layout)
         
-        self.counts = {"hair":0}
-        self.max_counts = {"hair":3}
+        self.counts = {"hair":0, "scars":0, "freckles":0, "jewelry":0}
+        self.max_counts = {"hair":3, "scars":6, "freckles":4, "jewelry":6}
         self.total_layers = 0
     
     def initContent(self, s):
@@ -78,8 +78,6 @@ class portraitStackWidget(QWidget):
         self.total_layers = len(self.parent.active_layers)
         self.parent.layer_orders[self.total_layers] = base
         
-        print(self.parent.layer_orders, self.parent.active_layers)
-        
         self.parent.layers_box.clear()
         self.parent.layers_box.addItems(self.parent.active_layers)
     
@@ -95,30 +93,89 @@ class portraitStackWidget(QWidget):
             self.total_layers = len(self.parent.active_layers)
             self.parent.layer_orders[self.total_layers] = hair
             
-            print(self.parent.layer_orders, self.parent.active_layers)
+            self.parent.layers_box.clear()
+            self.parent.layers_box.addItems(self.parent.active_layers)
+    
+    def initEyes(self):
+        ###REPLACE THIS###
+        eyes = "temp eyes"
+        
+        self.parent.active_layers["eyes"] = eyes
+        
+        self.total_layers = len(self.parent.active_layers)
+        self.parent.layer_orders[self.total_layers] = eyes
+        
+        self.parent.layers_box.clear()
+        self.parent.layers_box.addItems(self.parent.active_layers)
+    
+    def initNose(self):
+        ###REPLACE THIS###
+        nose = "temp nose"
+        
+        self.parent.active_layers["nose"] = nose
+        
+        self.total_layers = len(self.parent.active_layers)
+        self.parent.layer_orders[self.total_layers] = nose
+        
+        self.parent.layers_box.clear()
+        self.parent.layers_box.addItems(self.parent.active_layers)
+    
+    def initMouth(self):
+        ###REPLACE THIS###
+        mouth = "temp mouth"
+        
+        self.parent.active_layers["mouth"] = mouth
+        
+        self.total_layers = len(self.parent.active_layers)
+        self.parent.layer_orders[self.total_layers] = mouth
+        
+        self.parent.layers_box.clear()
+        self.parent.layers_box.addItems(self.parent.active_layers)
+    
+    def initScars(self):
+        self.counts["scars"] += 1
+        if self.counts["scars"] <= self.max_counts["scars"]:
+        
+            ###REPLACE THIS###
+            scar = "temp scar " + str(self.counts["scars"])
+            
+            self.parent.active_layers["scar " + str(self.counts["scars"])] = scar
+            
+            self.total_layers = len(self.parent.active_layers)
+            self.parent.layer_orders[self.total_layers] = scar
             
             self.parent.layers_box.clear()
             self.parent.layers_box.addItems(self.parent.active_layers)
-            
-            
-    
-    def initEyes(self):
-        pass
-    
-    def initNose(self):
-        pass
-    
-    def initMouth(self):
-        pass
-    
-    def initScars(self):
-        pass
     
     def initFreckles(self):
-        pass
+        self.counts["freckles"] += 1
+        if self.counts["freckles"] <= self.max_counts["freckles"]:
+        
+            ###REPLACE THIS###
+            freckle = "temp freckle " + str(self.counts["freckles"])
+            
+            self.parent.active_layers["freckle " + str(self.counts["freckles"])] = freckle
+            
+            self.total_layers = len(self.parent.active_layers)
+            self.parent.layer_orders[self.total_layers] = freckle
+            
+            self.parent.layers_box.clear()
+            self.parent.layers_box.addItems(self.parent.active_layers)
     
     def initJewelry(self):
-        pass
+        self.counts["jewelry"] += 1
+        if self.counts["jewelry"] <= self.max_counts["jewelry"]:
+        
+            ###REPLACE THIS###
+            jewelry = "temp jewelry " + str(self.counts["jewelry"])
+            
+            self.parent.active_layers["jewelry " + str(self.counts["jewelry"])] = jewelry
+            
+            self.total_layers = len(self.parent.active_layers)
+            self.parent.layer_orders[self.total_layers] = jewelry
+            
+            self.parent.layers_box.clear()
+            self.parent.layers_box.addItems(self.parent.active_layers)
     
     def initMasks(self):
         pass
