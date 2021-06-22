cf516c6f445caa4fd134fc8dcd67d0207ffba302
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-06-22 10:56:18 -0600

    app loader work

--- pygame_testing.py
+++ pygame_testing.py
@@ -1,20 +1,40 @@
-import pygame, sys
+import pygame, sys, random, json
 
-loader = "retro"
+class Constants():
+    def __init__(self,data):
+        super().__init__()
+        with open(data, "r") as f:
+            d = json.load(f)
+        self.loader = d[0]
+        self.sounds = d[1]
+        self.stickers = d[2]
+        self.stickers_dict = d[3]
+        self.aw = d[4]
+
+C = Constants("src/tmp/alc.trecd")
 
 class retroLoader():
     def __init__(self, dimensions, title, initial_bg, icon, bar_bg, cursor_speed):
         super().__init__()
         pygame.init()
+        pygame.mixer.init()
         self.initMainWindow(dimensions, title, initial_bg, icon, bar_bg, cursor_speed)
     
     def initInitialValues(self, dimensions, cursor_speed):
         self.cursor_pos = [192,64]
         self.cursor_x_change = 0
         self.cursor_y_change = 0
+        
         self.dimensions = dimensions
         self.last_cursor_move = pygame.time.get_ticks()
+        self.b_pos = {64:"load",256:"create",448:"settings"}
+        
+        self.move_sound = pygame.mixer.Sound("app/app_sounds/move.mp3")
+        self.select_sound = pygame.mixer.Sound("app/app_sounds/select.mp3")
+        
         self.cursor_move_cooldown = cursor_speed
+        clock = pygame.time.Clock()
+        clock.tick(60)
         
     def initMainWindow(self, dimensions, title, initial_bg, icon, bar_bg, cursor_speed):
         #load variables and constants
@@ -46,12 +66,16 @@ class retroLoader():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
                         self.cursor_y_change = -192
-                    if event.key == pygame.K_DOWN:
+                    elif event.key == pygame.K_DOWN:
                         self.cursor_y_change = 192
+                    elif event.key == pygame.K_RETURN:
+                        self.select_sound.play()
+                        self.buttonPress(self.b_pos[self.cursor_pos[1]])
 
                 #Key release
                 elif event.type == pygame.KEYUP:
                     if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
+                        self.move_sound.play()
                         self.cursor_x_change = 0
                         self.cursor_y_change = 0
                         
@@ -64,8 +88,12 @@ class retroLoader():
             self.fake_screen.fill(initial_bg)
             
             #self.show...
-            self.showButtons()
-            self.showCursor()
+            self.showConsole()
+            if C.aw == "main":
+                self.showButtons()
+                self.showCursor()
+            if C.aw == "settings":
+                self.showSettings()
             
             #screen.blit(pygame.transform.scale(self.fake_screen, screen.get_rect().size), (0, 0))
             if self.screen_rect.size != self.dimensions:
@@ -77,6 +105,20 @@ class retroLoader():
                 self.screen.blit(self.fake_screen, (0,0))
             
             pygame.display.update()
+            
+    def buttonPress(self, s):
+        if s == "settings":
+            C.aw = "settings"
+    
+    def showConsole(self):
+        self.leds = pygame.image.load("app/app_imgs/leds.png")
+        self.screen_img = pygame.image.load("app/app_imgs/screen.png")
+        self.fake_screen.blit(self.leds, (560,500))
+        self.fake_screen.blit(self.screen_img, (0,46))
+    
+    def showSettings(self):
+        #loader, sounds Y/N, stickers Y/N, re-arrange stickers
+        pass
     
     def showButtons(self):
         if self.cursor_pos[1] == 64:
@@ -92,15 +134,10 @@ class retroLoader():
         elif self.cursor_pos[1] != 448:
             self.settings_button = pygame.image.load("app/app_imgs/settings.png")
 
-        self.leds = pygame.image.load("app/app_imgs/leds.png")
-        self.screen_img = pygame.image.load("app/app_imgs/screen.png")
-        self.speaker = pygame.image.load("app/app_imgs/speaker.png")
-        self.fake_screen.blit(self.screen_img, (0,46))
         self.fake_screen.blit(self.load_button, (192,64))
         self.fake_screen.blit(self.create_button, (192,256))
         self.fake_screen.blit(self.settings_button, (192,448))
-        self.fake_screen.blit(self.leds, (560,500))
-        self.fake_screen.blit(self.speaker, (630,10))
+        
     
     def showCursor(self):
         img = pygame.image.load("app/app_imgs/cursor.png")
@@ -123,5 +160,5 @@ class retroLoader():
         imgY = self.cursor_pos[1]
         self.fake_screen.blit(img, (imgX,imgY))
 
-if loader == "retro":
+if C.loader == "retro":
     m = retroLoader((840,640), "Testing", "#DfDfDf", "icon.png", "#000000", 70)
