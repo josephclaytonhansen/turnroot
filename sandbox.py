97a368fa0cdf7364f13de421e44c2b3eaf2598b3
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-06-23 12:16:20 -0600

    sandbox map camera control

--- sandbox.py
+++ sandbox.py
@@ -8,6 +8,7 @@ class Constants():
         self.scale = d[0]
         self.fps = d[1]
         self.cursor_speed = d[2]
+        self.grid_dimensions = d[3]
 
 C = Constants("src/tmp/sc.trecd")
 C.scale = 64
@@ -45,8 +46,11 @@ class sandbox():
         self.cursor_pos = [0,0]
         self.tile_pos = [0,0]
         self.tile_offset = [0,0]
+        self.current_tile = [0,0]
         self.cursor_x_change = 0
         self.cursor_y_change = 0
+        self.cursor_history = []
+        self.moved = False
         
         self.dimensions = dimensions
         self.last_cursor_move = pygame.time.get_ticks()
@@ -68,10 +72,16 @@ class sandbox():
         #init screen
         screen = pygame.display.set_mode(self.dimensions, flags=(pygame.RESIZABLE))
         fake_screen = screen.copy()
+        self.camera = fake_screen.get_rect()
         self.fake_screen = fake_screen
         self.screen = screen
         self.icon = icon
         running = True
+        
+        if C.scale == 64:
+            self.c_img = pygame.image.load("app/app_imgs/64cursor.png")
+        elif C.scale == 32:
+            self.c_img = pygame.image.load("app/app_imgs/32cursor.png")
         
         self.fake_rect = self.fake_screen.get_rect()
         self.screen_rect = self.screen.get_rect()
@@ -90,13 +100,17 @@ class sandbox():
                 #Key press
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
+                        self.moved = True
                         self.cursor_y_change = -C.scale
                     elif event.key == pygame.K_DOWN:
                         self.cursor_y_change = C.scale
+                        self.moved = True
                     elif event.key == pygame.K_LEFT:
                         self.cursor_x_change = -C.scale
+                        self.moved = True
                     elif event.key == pygame.K_RIGHT:
                         self.cursor_x_change = C.scale
+                        self.moved = True
                     elif event.key == pygame.K_RETURN:
                         t = self.tiles[self.tile_pos[0]][self.tile_pos[1]]
                         print(t.x, t.y, t)
@@ -104,6 +118,8 @@ class sandbox():
                 #Key release
                 elif event.type == pygame.KEYUP:
                     if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
+                        self.cursor_history = []
+                        self.moved = False
                         self.cursor_x_change = 0
                         self.cursor_y_change = 0
                         
@@ -116,10 +132,10 @@ class sandbox():
             self.fake_screen.fill(initial_bg)
             
             #self.show...
-            self.graphics.draw(self.fake_screen)
-                
-                
+            self.tile_group.draw(self.fullmap)
+ 
             self.showCursor()
+            self.fake_screen.blit(self.fullmap, (0,0), self.camera)
             
             #screen.blit(pygame.transform.scale(self.fake_screen, screen.get_rect().size), (0, 0))
             if self.screen_rect.size != self.dimensions:
@@ -133,41 +149,56 @@ class sandbox():
             pygame.display.update()
             
     def initGrid(self):
+        self.fullmap = pygame.Surface((C.grid_dimensions[0]*C.scale, C.grid_dimensions[1]*C.scale))
+        self.fullmap_rect = self.fullmap.get_rect()
         max_x = int(self.dimensions[0] / C.scale)
         max_y = int(self.dimensions[1] / C.scale)
-        for x in range(0,max_x+1):
+        for x in range(0,C.grid_dimensions[0]+1):
             self.tiles[x] = {}
-            
-            for y in range(0,max_y+1):
+            for y in range(0,C.grid_dimensions[1]+1):
                 self.tiles[x][y] = Tile(x,y,0,"ground")
                 self.tile_group.add(self.tiles[x][y])
-                self.graphics.add(self.tiles[x][y])
+                self.graphics.add(self. tiles[x][y])
     
     def showCursor(self):
-        if C.scale == 64:
-            img = pygame.image.load("app/app_imgs/64cursor.png")
-        elif C.scale == 32:
-            img = pygame.image.load("app/app_imgs/32cursor.png")
         now = pygame.time.get_ticks()
+        
         if now - self.last_cursor_move >= self.cursor_move_cooldown:
             self.cursor_pos[0] += self.cursor_x_change
             self.cursor_pos[1] += self.cursor_y_change
-            if self.cursor_pos[0] < 0:
-                self.cursor_pos[0] = 0
-            if self.cursor_pos[0] >= self.dimensions[0] - C.scale:
-                self.cursor_pos[0] = self.dimensions[0] - C.scale
-            if self.cursor_pos[1] < 0:
-                self.cursor_pos[1] = 0
-            if self.cursor_pos[1] >= self.dimensions[1] - C.scale:
-                self.cursor_pos[1] = self.dimensions[1] - C.scale
             
+            if self.moved:
+                self.cursor_history.append([self.cursor_x_change,self.cursor_y_change])
+        
             self.tile_pos[0] = int(self.cursor_pos[0] / C.scale) + self.tile_offset[0]
             self.tile_pos[1] = int(self.cursor_pos[1] / C.scale) + self.tile_offset[1]
+
+            if self.cursor_pos[0] < 0:
+                self.cursor_pos[0] = 0
+            if self.tile_pos[0] >= C.grid_dimensions[0]-1:
+                self.cursor_pos[0] = (C.grid_dimensions[0]-1)*C.scale
+            if self.cursor_pos[1] < 0:
+                self.cursor_pos[1] = 0
+            if self.tile_pos[1] >= C.grid_dimensions[1]-1:
+                self.cursor_pos[1] = (C.grid_dimensions[1]-1)*C.scale
             
+            if len(self.cursor_history) > 3:
+                camera_pan_direction = self.cursor_history[0]
+                h_speed = C.scale
+                v_speed = C.scale
+                if camera_pan_direction[0] == -C.scale:
+                    self.camera.x -=h_speed
+                elif camera_pan_direction[0] == C.scale:
+                    self.camera.x +=h_speed
+                if camera_pan_direction[1] == C.scale:
+                    self.camera.y +=v_speed
+                elif camera_pan_direction[1] == -C.scale:
+                    self.camera.y -=v_speed
+
             self.last_cursor_move = now
             
         imgX = self.cursor_pos[0]
         imgY = self.cursor_pos[1]
-        self.fake_screen.blit(img, (imgX,imgY))
+        self.fullmap.blit(self.c_img, (imgX,imgY))
 
 m = sandbox((21*C.scale,13*C.scale), "Sandbox", "#FfFfFf", "icon.png", "#000000", C.cursor_speed)
