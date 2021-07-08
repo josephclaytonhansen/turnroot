de878c6577b70af397c683e297b7422b4722fb04
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-07-08 08:27:26 -0600

    clean up sandbox AI backend

--- src/GAME_battle_map_ai_backend.py
+++ src/GAME_battle_map_ai_backend.py
@@ -1,44 +1,3 @@
-# def circleToSquare(rows):
-#     width = -1
-#     widths = []
-#     full_widths = []
-#     paddings = []
-#     cells = []
-#     cells_split = []
-#     going_up = True
-#     done = False
-#     start = 1
-# 
-#     while not done:
-#         if width == rows:
-#             going_up = False
-#         if going_up:
-#             width += 2
-#         else:
-#             width -=2
-#         widths.append(width)
-#         full_widths.append(rows)
-#         paddings.append(int((rows-width)/2))
-#         if going_up == False and width == 1:
-#             done = True
-# 
-#     count = -1    
-#     for x in widths:
-#         count +=1
-#         for y in range(0,paddings[count]):
-#             cells.append("0")
-#         for l in range(0,widths[count]):
-#             cells.append("1")
-#         for y in range(0,paddings[count]):
-#             cells.append("0")
-# 
-#     for i in range(0, len(cells), rows):
-#         cells_split.append(cells[i:i + rows])
-#         
-#     return (cells_split,cells)
-
-#well that didn't work
-
 def circleToSquare(m,s,move_squares):
     cells = []
     cells_weight = []
@@ -59,5 +18,3 @@ def circleToSquare(m,s,move_squares):
 
 def gridWeight(l):
     pass
-#Get that WEIGHT
-    
