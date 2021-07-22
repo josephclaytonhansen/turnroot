3a82401c69e1c27952369d67aec48fd487b28fb6
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-07-22 12:28:59 -0600

    game editor error checking work

--- src/UI_game_editor_backend.py
+++ src/UI_game_editor_backend.py
@@ -3,6 +3,14 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import src.UI_colorTheme as UI_colorTheme
 from src.UI_updateJSON import updateJSON
+import fnmatch
+
+TOTAL_TASKS = 10
+
+FATAL_ERROR = "Fatal Error"
+WARNING = "Warning"
+CAUTION = "Caution"
+NO_ERROR = "No Error"
 
 def selectionRow(parent, query, options, colors, helpt):
     row = QWidget()
@@ -44,6 +52,7 @@ def selectionRow(parent, query, options, colors, helpt):
 class checkDialog(QDialog):
     def __init__(self,parent=None):
         data = updateJSON()
+        self.parent = parent
         self.active_theme = getattr(UI_colorTheme, data["active_theme"])
         super().__init__(parent)
         self.setStyleSheet("background-color: "+self.active_theme.window_background_color+";color: "+self.active_theme.window_text_color)
@@ -83,12 +92,87 @@ class checkDialog(QDialog):
         self.setLayout(layout)
         self.show()
     
+    def ERROR_CHECK(self, n):
+        #basic errors first- counts
+        #test unit count
+        if n == 0:
+            try:
+                dirpath = self.parent.game_path+"/units"
+                if len(fnmatch.filter(os.listdir(dirpath), '*.truf')) == 0:
+                   return [FATAL_ERROR, "No units found"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.truf')) < 12:
+                   return [WARNING, "Total units less than 12"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.truf')) < 24:
+                    return [CAUTION, "Total units less than 24"]
+                else:
+                    return [NO_ERROR]
+            except:
+                return [FATAL_ERROR, "No unit folder in game folder"]
+        #test class count
+        elif n == 1:
+            try:
+                dirpath = self.parent.game_path+"/classes"
+                if len(fnmatch.filter(os.listdir(dirpath), '*.tructf')) == 0:
+                   return [FATAL_ERROR, "No classes found"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.tructf')) < 10:
+                   return [WARNING, "Total classes less than 10"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.tructf')) < 20:
+                    return [CAUTION, "Total classes less than 20"]
+                else:
+                    return [NO_ERROR]
+            except:
+                return [FATAL_ERROR, "No class folder in game folder"]
+        #test weapon type count
+        elif n == 2:
+            try:
+                dirpath = self.parent.game_path+"/weapon_types"
+                if len(fnmatch.filter(os.listdir(dirpath), '*.json')) == 0:
+                   return [FATAL_ERROR, "No weapon types found"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.json')) < 3:
+                   return [WARNING, "Total weapon types less than 3"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.json')) < 5:
+                    return [CAUTION, "Total classes less than 5"]
+                else:
+                    return [NO_ERROR]
+            except:
+                return [FATAL_ERROR, "No weapon type folder in game folder"]
+        #test skills count
+        elif n == 3:
+            try:
+                dirpath = self.parent.game_path+"/skills"
+                if len(fnmatch.filter(os.listdir(dirpath), '*.trnep')) == 0:
+                   return [FATAL_ERROR, "No skills found"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.trnep')) < 7:
+                   return [WARNING, "Total skills less than 7"]
+                elif len(fnmatch.filter(os.listdir(dirpath), '*.trnep')) < 14:
+                    return [CAUTION, "Total skills less than 14"]
+                else:
+                    return [NO_ERROR]
+            except:
+                return [FATAL_ERROR, "No skills folder in game folder"]
+        #test items folder
+        elif n == 4:
+            try:
+                dirpath = self.parent.game_path+"/items"
+                return[NO_ERROR]
+            except:
+                return [FATAL_ERROR, "No items folder in game folder"]
+        else:
+            return[NO_ERROR]
+    
     def runTest(self):
         self.completed = 0
+        self.added_errors = []
         task_index = -1
-        while self.completed < 100:
+        while task_index < TOTAL_TASKS:
             task_index += 1
-            self.errors[ERROR_CHECK[task_index]] = ERROR_CHECK[task_index].run()
-            self.completed += 0.01
+            self.errors[task_index] = self.ERROR_CHECK(task_index)
+            self.completed += (100 / TOTAL_TASKS)
             self.progress.setValue(self.completed)
+            for k in self.errors:
+                print(k, self.errors[k])
+                if self.errors[k][0] != "No Error":
+                    if self.errors[k][1] not in self.added_errors:
+                        self.columns[self.errors[k][0]].addItem(self.errors[k][1])
+                        self.added_errors.append(self.errors[k][1])
             qApp.processEvents()
