947f2859a52af83e1a927609a877725b2fca4e7d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-07-27 15:58:27 -0600

    weapon triangle dialog visual shell

--- src/UI_game_editor_dialogs.py
+++ src/UI_game_editor_dialogs.py
@@ -13,7 +13,7 @@ class weaponTriangle(QDialog):
         super().__init__(parent)
         self.setStyleSheet("font-size: "+str(data["font_size"])+"px; background-color: white;color:black;")
 
-        self.setFixedSize(1000,500)
+        self.setFixedSize(1000,450)
         
         self.back = QLabel("",self)
         self.back.setPixmap(QPixmap("src/ui_images/triangle.png"))
@@ -30,16 +30,74 @@ class weaponTriangle(QDialog):
         self.wt = QPushButton(QIcon(), "Edit Weapon Types", self)
         self.wt.setFont(parent.body_font)
         self.wt.setMinimumHeight(48)
+        self.wt.setMinimumWidth(280)
         self.wt.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
         self.wt.clicked.connect(self.ok_clicked)
         
+        self.top_1 = QComboBox(self)
+        self.top_1.setFont(parent.body_font)
+        self.top_1.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
+        self.top_2 = QComboBox(self)
+        self.top_2.setFont(parent.body_font)
+        self.top_2.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
+        self.right_1 = QComboBox(self)
+        self.right_1.setFont(parent.body_font)
+        self.right_1.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
+        self.right_2 = QComboBox(self)
+        self.right_2.setFont(parent.body_font)
+        self.right_2.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
+        self.left_1 = QComboBox(self)
+        self.left_1.setFont(parent.body_font)
+        self.left_1.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
+        self.left_2 = QComboBox(self)
+        self.left_2.setFont(parent.body_font)
+        self.left_2.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
+        self.outliers_list = QListView(self)
+        self.outliers_list.setFont(parent.body_font)
+        self.outliers_list.setMaximumWidth(200)
+        self.outliers_list.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        
         self.ok.show()
-        self.ok.move(900, 420)
+        self.ok.move(900, 380)
         self.ok.raise_()
         
         self.wt.show()
-        self.wt.move(730, 420)
+        self.wt.move(70, 380)
         self.wt.raise_()
+        
+        self.top_1.show()
+        self.top_1.move(270, 50)
+        self.top_1.raise_()
+        
+        self.top_2.show()
+        self.top_2.move(360, 50)
+        self.top_2.raise_()
+        
+        self.right_1.show()
+        self.right_1.move(480, 300)
+        self.right_1.raise_()
+        
+        self.right_2.show()
+        self.right_2.move(570, 300)
+        self.right_2.raise_()
+        
+        self.left_1.show()
+        self.left_1.move(70, 300)
+        self.left_1.raise_()
+        
+        self.left_2.show()
+        self.left_2.move(160, 300)
+        self.left_2.raise_()
+        
+        self.outliers_list.show()
+        self.outliers_list.move(770, 50)
+        self.outliers_list.raise_()
 
         self.show()
         
