e58ba8f11f0562c57b14055034cf5f745f599765
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-07-28 21:20:44 -0600

    weapon triangle functional

--- src/UI_game_editor_dialogs.py
+++ src/UI_game_editor_dialogs.py
@@ -4,14 +4,18 @@ from PyQt5.QtGui import QColor, QPalette, QIcon, QPixmap, QCursor, QFont
 from src.UI_updateJSON import updateJSON
 import src.UI_colorTheme
 import shutil, os, pickle, json, sys
+from src.game_directory import gameDirectory
+from src.UI_unit_editor_more_dialogs import editUniversalWeaponTypes
 
 class weaponTriangle(QDialog):
     def __init__(self, parent=None):
         data = updateJSON()
+        self.load_data = None
         self.active_theme = getattr(src.UI_colorTheme, data["active_theme"])
         active_theme = self.active_theme
         super().__init__(parent)
-        self.setStyleSheet("font-size: "+str(data["font_size"])+"px; background-color: white;color:black;")
+        self.parent = parent
+        self.setStyleSheet("font-size: "+str(data["font_size"])+"px; background-color:"+active_theme.window_background_color+";color:"+active_theme.window_text_color+";")
 
         self.setFixedSize(1000,450)
         
@@ -32,40 +36,70 @@ class weaponTriangle(QDialog):
         self.wt.setMinimumHeight(48)
         self.wt.setMinimumWidth(280)
         self.wt.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
-        self.wt.clicked.connect(self.ok_clicked)
+        self.wt.clicked.connect(self.wt_clicked)
         
         self.top_1 = QComboBox(self)
+        self.top_1.name = "top_1"
         self.top_1.setFont(parent.body_font)
         self.top_1.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
         
         self.top_2 = QComboBox(self)
+        self.top_2.name = "top_2"
         self.top_2.setFont(parent.body_font)
-        self.top_2.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        self.top_2.setStyleSheet("background-color: "+active_theme.node_grid_background_color+";color:"+active_theme.window_text_color+";")
         
         self.right_1 = QComboBox(self)
+        self.right_1.name = "right_1"
         self.right_1.setFont(parent.body_font)
         self.right_1.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
         
         self.right_2 = QComboBox(self)
+        self.right_2.name = "right_2"
         self.right_2.setFont(parent.body_font)
-        self.right_2.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        self.right_2.setStyleSheet("background-color: "+active_theme.node_grid_background_color+";color:"+active_theme.window_text_color+";")
         
         self.left_1 = QComboBox(self)
+        self.left_1.name = "left_1"
         self.left_1.setFont(parent.body_font)
         self.left_1.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
         
         self.left_2 = QComboBox(self)
+        self.left_2.name = "left_2"
         self.left_2.setFont(parent.body_font)
-        self.left_2.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        self.left_2.setStyleSheet("background-color: "+active_theme.node_grid_background_color+";color:"+active_theme.window_text_color+";")
         
-        self.outliers_list = QListView(self)
+        self.outliers_list = QListWidget(self)
+        self.outliers_list.name = "outliers_list"
         self.outliers_list.setFont(parent.body_font)
         self.outliers_list.setMaximumWidth(200)
         self.outliers_list.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
         
+        self.lists = [self.top_1,self.top_2,self.right_1,self.right_2,self.left_1,self.left_2]
+        for k in self.lists:
+            k.currentTextChanged.connect(self.assign)
+        
+        self.outliers_label = QLabel(self)
+        self.outliers_label.setText("Outliers (No Advantage)")
+        self.outliers_label.setFont(parent.body_font)
+        
+        self.triangle_label = QLabel(self)
+        self.triangle_label.setText("Weapon Advantages (Physical: Left, Magic: Right)")
+        self.triangle_label.setToolTip("Physical weapons and magic have different triangles. Keep physical weapons in the left triangle and magic on the right.")
+        self.triangle_label.setFont(parent.body_font)
+        
+        self.reset_button = QPushButton(QIcon(), "Reset", self)
+        self.reset_button.setFont(parent.body_font)
+        self.reset_button.setMinimumHeight(48)
+        self.reset_button.setStyleSheet("background-color: "+active_theme.list_background_color+";color:"+active_theme.window_text_color+";")
+        self.reset_button.clicked.connect(self.reset_clicked)
+        
         self.ok.show()
         self.ok.move(860, 380)
         self.ok.raise_()
+        
+        self.reset_button.show()
+        self.reset_button.move(770, 380)
+        self.reset_button.raise_()
         
         self.wt.show()
         self.wt.move(70, 380)
@@ -98,10 +132,86 @@ class weaponTriangle(QDialog):
         self.outliers_list.show()
         self.outliers_list.move(770, 50)
         self.outliers_list.raise_()
-
-        self.show()
         
+        self.outliers_label.show()
+        self.outliers_label.move(770,20)
+        self.outliers_label.raise_()
+        
+        self.triangle_label.show()
+        self.triangle_label.move(100,20)
+        self.triangle_label.raise_()
+        
+        self.loadWT()
+        self.populateDD()
+        self.Load()
+        self.show()
+    
+    def loadWT(self):
+        with open("src/skeletons/universal_weapon_types.json", "r") as weapons_file:
+            self.wt_list = []
+            weapon_types = []
+            weapon_types = json.load(weapons_file)
+            self.wt_list = weapon_types.copy()
+            self.outliers_list.addItems(weapon_types)
+    
+    def populateDD(self):
+        with open("src/skeletons/universal_weapon_types.json", "r") as weapons_file:
+            weapon_types = ["--Select--"]
+            for x in json.load(weapons_file):
+                if x not in weapon_types:
+                    weapon_types.append(x)
+            for k in self.lists:
+                k.addItems(weapon_types)
+                    
+    def assign(self, s):
+        if s != "--Select--":
+            for l in self.lists:
+                if l.name != self.sender().name:
+                    try:
+                        l.removeItem(l.findText(s))
+                        self.wt_list.remove(s)
+                        self.outliers_list.clear()
+                        self.outliers_list.addItems(self.wt_list)
+                        
+                    except Exception as e:
+                        pass
+    
     def ok_clicked(self):
         self.return_confirm = True
+        self.Save()
         self.close()
-
+        
+    def wt_clicked(self):
+        y = editUniversalWeaponTypes(self,self.parent.body_font)
+        y.exec_()
+        
+    def reset_clicked(self):
+        for l in self.lists:
+            l.clear()
+            self.outliers_list.clear()
+        self.populateDD()
+        self.loadWT()
+    
+    def Load(self):
+        g = gameDirectory(self)
+        g.getPath()
+        with open(g.path+"/weapon_triangle.trsl", "r") as f:
+            self.load_data = json.load(f)
+            print(self.load_data)
+        assigns = [self.left_1, self.left_2,
+                 self.top_1, self.top_2,
+                 self.right_1,self.right_2]
+        try:
+            for i in self.load_data:
+                    assigns[self.load_data.index(i)].setCurrentText(i)
+        except Exception as e:
+            print(e)
+    
+    def Save(self):
+        order = [self.left_1.currentText(), self.left_2.currentText(),
+                 self.top_1.currentText(), self.top_2.currentText(),
+                 self.right_1.currentText(),self.right_2.currentText()]
+        g = gameDirectory(self)
+        g.getPath()
+        with open(g.path+"/weapon_triangle.trsl", "w") as f:
+            json.dump(order, f)
