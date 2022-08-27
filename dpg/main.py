b9dd5fd1831c386b6ac6607ed0f3fec5c10f9cfe
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2022-08-27 16:07:43 -0500

    make fonts global

--- dpg/main.py
+++ dpg/main.py
@@ -3,7 +3,6 @@ from unit_editor import add_unit_editor
 import dearpygui.dearpygui as dpg
 from ui_set_global_colors import set_colors
 from ui_set_global_font import set_fonts
-import ui_colorthemes
 from globals import globals as g
 
 dpg.create_context()
@@ -12,8 +11,6 @@ dpg.bind_theme(set_colors(g.color_theme))
 set_fonts()
 init_viewport(True)
 add_unit_editor()
-
-
 
 if not g.debug:
     dpg.set_primary_window("unit_editor", True)
--- dpg/ui_set_global_font.py
+++ dpg/ui_set_global_font.py
@@ -3,6 +3,6 @@ from globals import globals as g
 
 def set_fonts(label="Assets/Fonts/FiraSans-Regular.ttf", mono="Assets/Fonts/FiraCode-Regular.ttf"):
     with dpg.font_registry():
-        default_font = dpg.add_font(label, 20)
-        monospace = dpg.add_font(mono, 20)
-        dpg.bind_font(default_font)
+        g.default_font = dpg.add_font(label, 20)
+        g.monospace = dpg.add_font(mono, 20)
+        dpg.bind_font(g.default_font)
